ff425bba99193ab376d4c8b947f5fff68f75ba3d
araba <34araba@gmail.com>
2017-01-23 21:48:04 +0100

    adding application helper and layouts application

--- appli/app/helpers/application_helper.rb
+++ appli/app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #permet de ne pas laisser de titre vide 
+  def titre
+    base_titre = "Application"
+    if @titre.nil?
+      base_titre
+    else
+      @titre
+    end
+  end
 end
